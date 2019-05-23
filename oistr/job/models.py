c6d4966dc2e3273583dc37f2e09f38460a9721ea
Kristófer Gudmundsson <kristofergudmundsson@Kristofers-MacBook-Pro.local>
2019-05-23 14:00:54 +0000

    about to test migrations

--- oistr/job/models.py
+++ oistr/job/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Job(models.Model):
+    company = models.CharField(max_length=255, default='Company')
+    title = models.CharField(max_length=255, default='Title')
+    description = models.CharField(max_length=990, default='Description')
+    requirements = models.CharField(max_length=990, default='Requirements')
