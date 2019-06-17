cb4b3233b44228ed80502c39bd3463d2433d85d5
stofistofi <kristofer17@ru.is>
2019-06-17 18:56:32 +0000

    detail pages, rought

--- oistr/job/views.py
+++ oistr/job/views.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 
 # Create your views here.
 from job.forms.job_form import JobForm
+from job.models import Job
 
 
 @login_required
@@ -23,3 +24,11 @@ def add_job(request):
         # The page is initially loaded with a blank form
         context = { 'job_form': JobForm()}
         return render(request, 'job/add-job.html', context)
+
+@login_required
+def job_details(request, job_id):
+    job = get_object_or_404(Job, pk=job_id)
+    #if request.user.is_authenticated:
+
+    context = { 'job': job}
+    return render(request, 'job/job-details.html', context)
