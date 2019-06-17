d132c9a103d20824d48ca20d83b84d8be1773a54
stofistofi <kristofer17@ru.is>
2019-06-17 17:43:29 +0000

    add job form

--- oistr/job/views.py
+++ oistr/job/views.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
+
+'''
+@login_required
+def add_job(request):
+    if request.method == 'POST':
+'''
